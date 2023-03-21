aa1a6ad583d794b8137ce09d9f3e08f999714361
pedroviniciusn <pedrovinicius.dev@gmail.com>
2023-03-21 13:37:52 -0300

    feat: Alterando estado

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,13 +1,30 @@
-import { Reducer } from 'redux';
-import { ICarState } from './types';
+import { Reducer } from "redux";
+import { ICarState } from "./types";
 
 const INITIAL_STATE: ICarState = {
   items: [],
-}
+};
 
-const cart: Reducer<ICarState> = (state, action) => {
-  console.log(state, action)
-  return INITIAL_STATE;
-}
+const cart: Reducer<ICarState> = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case "ADD_PRODUCT_TO_CART": {
+      const { product } = action.payload;
+
+      return {
+        ...state,
+        items: [
+          ...state.items,
+          {
+            product,
+            quantity: 1,
+          },
+        ],
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
 
 export default cart;
