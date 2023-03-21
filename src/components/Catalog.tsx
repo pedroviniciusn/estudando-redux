3ed756d136c12da285a12190999fa8806bcb06d4
pedroviniciusn <pedrovinicius.dev@gmail.com>
2023-03-21 18:18:57 -0300

    feat: Adicionando componente CatalogItem

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,11 +1,9 @@
-import { useCallback, useEffect, useState } from "react";
-import { useDispatch } from 'react-redux';
-import api from "../services/api";
-import { addProductsToCart } from '../store/modules/cart/actions';
+import { useEffect, useState } from "react";
+import CatalogItem from "./CatalogItem";
 import { IProduct } from "../store/modules/cart/types";
+import api from "../services/api";
 
 function Catalog() {
-  const dispatch = useDispatch();
   const [catalog, setCatalog] = useState<IProduct[]>([]);
 
   useEffect(() => {
@@ -14,22 +12,12 @@ function Catalog() {
     });
   }, []);
 
-  const handleAddToProductToCart = useCallback((product: IProduct) => {
-    dispatch(addProductsToCart(product));
-  }, [dispatch]);
-
   return (
     <>
       <h1>Catalog</h1>
 
       {catalog.map((item) => {
-        return (
-          <article key={item.id}>
-            <strong>{item.title}</strong> {" - "}
-            <span>{item.price}</span> {"  "}
-            <button type="button" onClick={() => handleAddToProductToCart(item)}>Comprar</button>
-          </article>
-        );
+        return <CatalogItem key={item.id} product={item} />;
       })}
     </>
   );
