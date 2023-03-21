3a71328e7129f0bb91d6342678edc98ca26c2a64
pedroviniciusn <pedrovinicius.dev@gmail.com>
2023-03-21 19:14:16 -0300

    feat: Obtendo quantidade do carrinho

--- src/store/modules/cart/sagas.ts
+++ src/store/modules/cart/sagas.ts
@@ -1,7 +1,20 @@
-import { all, takeLatest } from "redux-saga/effects";
+import { all, select, takeLatest } from "redux-saga/effects";
+import { IState } from "../..";
+import { addProductsToCart } from "./actions";
 
-function checkProductStock() {
-  console.log("Adicionou ao carrinho");
+type CheckProductStockRequest = ReturnType<typeof addProductsToCart>;
+
+function* checkProductStock({ payload, type }: CheckProductStockRequest) {
+  const { product } = payload;
+
+  const currentQuantity: number = yield select((state: IState) => {
+    return (
+      state.cart.items.find((item) => item.product.id === product.id)
+        ?.quantity ?? 0
+    );
+  });
+
+  console.log(currentQuantity)
 }
 
 export default all([takeLatest("ADD_PRODUCT_TO_CART", checkProductStock)]);
